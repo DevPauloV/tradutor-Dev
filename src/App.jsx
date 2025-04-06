144236777324e17a5dee4db22f385fcf3d7504fe
Paulo Victor <vitorcanalle@gmail.com>
2025-04-06 17:27:37 -0300

    Testando funcionalidades do Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,31 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Tradutor</h1>
+    <div className='min-h-screen bg-background flex flex-col'>
+
+
+
+      <header className='bg-white shadow-sm'>
+        <div className='max-w-5xl mx-auto px-4 py-3 flex items-center'>
+          <h1 className='text-headerColor text-2xl font-bold'>Tradutor</h1>
+        </div>
+      </header>
+
+
+<main className='flex-grow flex items-start justify-center px-4 py-8'>
+<div className='w-full max-w-5xl bg-white rounded-lg shadow-md overflow-hidden'>
+  <div>
+
+  </div>
+</div>
+</main>
+
+
+
+
+
     </div>
   )
 }
